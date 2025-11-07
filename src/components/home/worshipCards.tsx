4935ba7c6d72b77631963c53fe4f95d67e636abf
SPEED OF LIGHT <mac@MacBookPro.lan>
2025-11-07 06:29:10 -0800

    service time  section info update

--- src/components/home/worshipCards.tsx
+++ src/components/home/worshipCards.tsx
@@ -10,17 +10,17 @@ const CardData = [
     description: "Join in our worship service every Sunday",
     img: "/home/PHOTO-2024-04-08-10-18-11.jpg",
     time: {
-      victory: "Sunday - 11AM",
-      restoration: "Sunday - 4PM",
+      USA: "Sunday - 11AM",
+      owerri: "Sunday - 4PM",
     },
   },
   {
-    title: "Prayer Serivce",
+    title: "Prayer Serivce (VIA ZOOM)",
     description: "Join in our worship service every Sunday",
     img: "/home/IMG-20250825-WA0131.jpg",
     time: {
-      victory: "Monday - 9PM (ZOOM)",
-      restoration: "",
+      USA: "Monday - 9PM",
+      owerri: "Tuesday - 2AM",
     },
   },
   {
@@ -28,17 +28,17 @@ const CardData = [
     description: "Join in our worship service every Sunday",
     img: "/home/IMG-20250825-WA0083.jpg",
     time: {
-      victory: "Wednesday - 7PM",
-      restoration: "Thursday - 5PM",
+      USA: "Wednesday - 7PM",
+      owerri: "Thursday - 5PM",
     },
   },
   {
-    title: "Midinght Prayer Fire Night",
+    title: "Midinght Prayer - Fire Night (via Zoom)",
     description: "Join in our worship service every Sunday",
     img: "/home/DSC_0249 copy.jpg",
     time: {
-      victory: "Friday - 11PM",
-      restoration: "",
+      USA: "Friday - 11PM",
+      owerri: "Saturday - 5AM",
     },
   },
 ];
@@ -54,7 +54,7 @@ export function WorshipCards({scripture}:{scripture?: boolean}) {
     <>
     <div
       ref={ref}
-      className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8"
+      className="grid grid-cols-1 md:grid-cols-2 gap-8"
     >
       {CardData.map((card, index) => {
         return (
@@ -75,23 +75,23 @@ export function WorshipCards({scripture}:{scripture?: boolean}) {
               />
             </div>
 
-            <div className="flex gap-2 px-4 py-6 items-center">
-              <div className="h-16 w-1 bg-primary-900"></div>
+            <div className="flex gap-2 px-4 py-6">
+              <div className="h-20 w-1 bg-primary-900"></div>
 
               <div className="flex flex-col  gap-2">
-                <h2 className="text-dark font-medium uppercase">
+                <h2 className="text-dark font-medium uppercase text-md">
                   {card.title}
                 </h2>
 
                 <div className="space-y-1">
-                  {card.time.victory && (
-                    <p className="text-primary-900 text-xs font-extrabold">
-                      {`Victory Chapel: ${card.time.victory}`}
+                  {card.time.USA && (
+                    <p className="text-primary-900 text-xs sm:text-sm font-bold border-b pb-2">
+                      {`Restoration Chapel - USA: ${card.time.USA}`}
                     </p>
                   )}
-                  {card.time.restoration && (
-                    <p className="text-primary-900 text-xs font-extrabold">
-                      {`Restoration Chapel: ${card.time?.restoration}`}
+                  {card.time.owerri && (
+                    <p className="text-primary-900 text-xs sm:text-sm font-bold">
+                      {`Restoration Chapel - Owerri: ${card.time?.owerri}`}
                     </p>
                   )}
                 </div>
